30353310648fe60cf9d293e21181df801586f267
Antonino Bongiovanni <antoniobongio@gmail.com>
2021-06-23 18:30:30 +0200

    Added SISLTB.h

---
+++ include/sisl_toolbox/SISLTB.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "curve.hpp"
+#include "straight_line.hpp"
+#include "circular_arc.hpp"
+#include "generic_curve.hpp"
+
+#include "path.hpp"
+#include "path_factory.hpp"
+
+#include "persistence_manager.hpp"
